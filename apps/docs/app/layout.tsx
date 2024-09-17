71eb0de8d9aed5bd31743670776a5fc391bd9674
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-09-18 02:49:25 +0700

    #fix: thuanvv fix 18-9-v2

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -8,62 +8,62 @@ import { ViewTransitions } from "next-view-transitions";
 import Providers from "./provider";
 
 const MainContainer = dynamic(() => import("@shared/layouts/main/container"), {
-    ssr: false,
+  ssr: false,
 });
 
 export const nextFont = Edu_VIC_WA_NT_Beginner({
-    display: "swap",
-    weight: ["500", "600", "700"],
-    preload: true,
-    subsets: ["latin"],
+  display: "swap",
+  weight: ["500", "600", "700"],
+  preload: true,
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Austin Vu Web",
-    description: "Austin Vu",
-    icons: [
-        {
-            rel: "apple-touch-icon",
-            url: "/logo.png",
-        },
-        {
-            rel: "icon",
-            type: "image/png",
-            sizes: "32x32",
-            url: "/logo.png",
-        },
-        {
-            rel: "icon",
-            type: "image/png",
-            sizes: "16x16",
-            url: "/logo.png",
-        },
-        {
-            rel: "icon",
-            url: "logo.png",
-        },
-    ],
+  title: "Austin Vu Web",
+  description: "Austin Vu",
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      url: "/logo.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/logo.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/logo.png",
+    },
+    {
+      rel: "icon",
+      url: "logo.png",
+    },
+  ],
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        // <ViewTransitions>
-        <html lang="en" suppressHydrationWarning>
-            <body
-                className={cn(
-                    "transition ease-in-out duration-300 antialiased",
-                    nextFont.className,
-                )}
-            >
-                <Providers>
-                    <MainContainer>{children}</MainContainer>
-                </Providers>
-            </body>
-        </html>
-        // </ViewTransitions>
-    );
+  return (
+    // <ViewTransitions>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "transition ease-in-out duration-300 antialiased",
+          nextFont.className,
+        )}
+      >
+        <Providers>
+          <MainContainer>{children}</MainContainer>
+        </Providers>
+      </body>
+    </html>
+    // </ViewTransitions>
+  );
 }
