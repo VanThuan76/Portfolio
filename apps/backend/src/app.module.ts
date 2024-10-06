4c4a32276df19ab811aeb417156681cc12d154e8
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-10-06 23:23:06 +0700

    #delpoy: thuanvv deploy be-v4

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -8,14 +8,18 @@ import { ProjectModule } from '@/shared/project/project.module';
 import { InformationWorkModule } from '@/shared/information-task/information-work.module';
 import { BlogModule } from '@/shared/blog/blog.module';
 
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 @Module({
-  imports: [
-    EnvModule,
-    SupabaseModule,
-    TagsModule,
-    ProjectModule,
-    BlogModule,
-    InformationWorkModule,
-  ],
+    imports: [
+        EnvModule,
+        SupabaseModule,
+        TagsModule,
+        ProjectModule,
+        BlogModule,
+        InformationWorkModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
