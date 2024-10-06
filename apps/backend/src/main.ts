6bd3e5c974d44df378b601e747bf63b74001e5e7
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-10-06 22:42:12 +0700

    #delpoy: thuanvv deploy be-v3

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,35 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import {
-    FastifyAdapter,
-    NestFastifyApplication,
+  FastifyAdapter,
+  NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { ValidationError } from '@nestjs/class-validator';
-import { Logger, PreconditionFailedException, ValidationPipe } from '@nestjs/common';
+import {
+  Logger,
+  PreconditionFailedException,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(
-        AppModule,
-        new FastifyAdapter(),
-    );
-    const logger = new Logger(bootstrap.name);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const logger = new Logger(bootstrap.name);
 
-    app.enableShutdownHooks();
+  app.enableShutdownHooks();
 
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: true,
-            transform: true,
-            enableDebugMessages: true,
-            exceptionFactory: (validationErrors: ValidationError[] = []) => {
-                throw new PreconditionFailedException(validationErrors);
-            },
-        }),
-    );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      enableDebugMessages: true,
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        throw new PreconditionFailedException(validationErrors);
+      },
+    }),
+  );
 
-    await app.listen(process.env.PORT || 8000, async () => {
-        const url = await app.getUrl();
-        logger.log(`Application is running on: ${url}`);
-    });
+  await app.listen(process.env.PORT || 8000, async () => {
+    const url = await app.getUrl();
+    logger.log(`Application is running on: ${url}`);
+  });
 }
 bootstrap();
