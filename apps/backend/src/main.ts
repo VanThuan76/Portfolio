b03e6e9e342842123f06eb79fba27b874d392d2b
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-10-06 22:40:43 +0700

    #delpoy: thuanvv deploy be-v3

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,16 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import {
-  FastifyAdapter,
-  NestFastifyApplication,
+    FastifyAdapter,
+    NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ValidationError } from '@nestjs/class-validator';
+import { Logger, PreconditionFailedException, ValidationPipe } from '@nestjs/common';
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  await app.listen(8000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
+    const logger = new Logger(bootstrap.name);
+
+    app.enableShutdownHooks();
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            enableDebugMessages: true,
+            exceptionFactory: (validationErrors: ValidationError[] = []) => {
+                throw new PreconditionFailedException(validationErrors);
+            },
+        }),
+    );
+
+    await app.listen(process.env.PORT || 8000, async () => {
+        const url = await app.getUrl();
+        logger.log(`Application is running on: ${url}`);
+    });
 }
 bootstrap();
