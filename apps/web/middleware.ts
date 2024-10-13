b7c584630976b01e9a082dc31072417f2af56ae4
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-10-14 00:55:27 +0700

    #update: thuanvv update repo v2-1

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -2,41 +2,41 @@ import createMiddleware from "next-intl/middleware";
 import { NextRequest } from "next/server";
 import { updateSession } from "@shared/utils/supabase/middlerware";
 
-export async function middleware(request: NextRequest) {
-  const response = await updateSession(request);
-
-  const localeMiddleware = createMiddleware({
+const localeMiddleware = createMiddleware({
     locales: ["en", "vi", "ja"],
     defaultLocale: "en",
     localeDetection: false,
     localePrefix: "always",
-  });
+});
 
-  if (!response) {
-    return localeMiddleware(request);
-  }
+export async function middleware(request: NextRequest) {
+    const response = await updateSession(request);
+    const localeResponse = localeMiddleware(request);
 
-  const localeResponse = localeMiddleware(request);
+    if (!response) {
+        return localeResponse;
+    }
 
-  response.headers.forEach((value, key) => {
-    localeResponse.headers.set(key, value);
-  });
+    response.headers.forEach((value, key) => {
+        localeResponse.headers.set(key, value);
+    });
 
-  return localeResponse;
+    return localeResponse;
 }
 
+
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - .glb files (3D models)
-     * - .mp3 and .mp4 files (audio and video)
-     */
-    "/",
-    "/(vi|en|ja)/:path*",
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|glb|mp3|mp4)$).*)",
-  ],
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - .glb files (3D models)
+         * - .mp3 and .mp4 files (audio and video)
+         */
+        "/",
+        "/(vi|en|ja)/:path*",
+        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|glb|mp3|mp4)$).*)",
+    ],
 };
