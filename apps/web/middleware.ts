857aa1c187825cd1bcae86125abf9f1041a90076
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-10-14 00:58:20 +0700

    #update: thuanvv update repo v2-1

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,40 +3,39 @@ import { NextRequest } from "next/server";
 import { updateSession } from "@shared/utils/supabase/middlerware";
 
 const localeMiddleware = createMiddleware({
-    locales: ["en", "vi", "ja"],
-    defaultLocale: "en",
-    localeDetection: false,
-    localePrefix: "always",
+  locales: ["en", "vi", "ja"],
+  defaultLocale: "en",
+  localeDetection: false,
+  localePrefix: "always",
 });
 
 export async function middleware(request: NextRequest) {
-    const response = await updateSession(request);
-    const localeResponse = localeMiddleware(request);
+  const response = await updateSession(request);
+  const localeResponse = localeMiddleware(request);
 
-    if (!response) {
-        return localeResponse;
-    }
-
-    response.headers.forEach((value, key) => {
-        localeResponse.headers.set(key, value);
-    });
-
+  if (!response) {
     return localeResponse;
+  }
+
+  response.headers.forEach((value, key) => {
+    localeResponse.headers.set(key, value);
+  });
+
+  return localeResponse;
 }
 
-
 export const config = {
-    matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * - .glb files (3D models)
-         * - .mp3 and .mp4 files (audio and video)
-         */
-        "/",
-        "/(vi|en|ja)/:path*",
-        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|glb|mp3|mp4)$).*)",
-    ],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - .glb files (3D models)
+     * - .mp3 and .mp4 files (audio and video)
+     */
+    "/",
+    "/(vi|en|ja)/:path*",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|glb|mp3|mp4)$).*)",
+  ],
 };
