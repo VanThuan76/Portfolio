bc6ea5d6fad14ce382f48a5c76f61f98a700a44c
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-10-17 21:22:29 +0700

    #update: thuanvv update repo v2-9

--- packages/ui/src/threejs/index.tsx
+++ packages/ui/src/threejs/index.tsx
@@ -6,33 +6,33 @@ import { Preload, Stats } from "@react-three/drei";
 import Experience from "./experience";
 
 export default function App({ ...props }) {
-  const isMobileOrIpad = ["xs", "sm"].includes(props.breakpoint);
+    const isMobileOrIpad = ["xs", "sm"].includes(props.breakpoint);
 
-  return (
-    <Canvas
-      shadows
-      dpr={isMobileOrIpad ? 1 : [1, 1.5]}
-      camera={{ near: 1, fov: 100 }}
-      eventPrefix="client"
-      gl={{ antialias: true }}
-      onCreated={({ gl }) => {
-        gl.toneMapping = THREE.NoToneMapping;
-      }}
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        pointerEvents: "none",
-      }}
-      {...props}
-    >
-      <Preload all />
-      <Suspense fallback={null}>
-        <Experience positions={props.positions} breakpoint={props.breakpoint} />
-      </Suspense>
-      <Stats showPanel={0} />
-    </Canvas>
-  );
+    return (
+        <Canvas
+            shadows
+            dpr={isMobileOrIpad ? 1 : [1, 1.5]}
+            camera={{ near: 1, fov: 100 }}
+            eventPrefix="client"
+            gl={{ antialias: true }}
+            onCreated={({ gl }) => {
+                gl.toneMapping = THREE.NoToneMapping;
+            }}
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100vw",
+                height: "100vh",
+                pointerEvents: "none",
+            }}
+            {...props}
+        >
+            <Preload />
+            <Suspense fallback={null}>
+                <Experience positions={props.positions} breakpoint={props.breakpoint} />
+            </Suspense>
+            <Stats showPanel={0} />
+        </Canvas>
+    );
 }
