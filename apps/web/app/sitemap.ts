76d103d88a2ec2bbca73b2361414876c4ca7af33
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-10-24 23:07:05 +0700

    #update: thuanvv update v1

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -1,39 +1,39 @@
 import {
-    getBlogCategories,
-    getBlogs,
+  getBlogCategories,
+  getBlogs,
 } from "@shared/query/actions/blog-actions";
 import { getSupabaseBrowserClient } from "@shared/utils/supabase/client";
 
 export default async function sitemap() {
-    const supabase = getSupabaseBrowserClient();
-    const baseUrl = "https://www.austinvu.tech";
+  const supabase = getSupabaseBrowserClient();
+  const baseUrl = "https://www.austinvu.tech";
 
-    const locales = ["en", "vi", "ja"];
+  const locales = ["en", "vi", "ja"];
 
-    const sitemapUrls: { url: string; lastModified: Date }[] = [];
-    for (const locale of locales) {
-        const { data: posts } = await getBlogs(supabase, locale);
-        if (!posts) throw new Error("Error fetching posts");
+  const sitemapUrls: { url: string; lastModified: Date }[] = [];
+  for (const locale of locales) {
+    const { data: posts } = await getBlogs(supabase, locale);
+    if (!posts) throw new Error("Error fetching posts");
 
-        const postsUrls = posts.map((post) => ({
-            url: `${baseUrl}/blog/${locale}/${post.slug}`,
-            lastModified: new Date(),
-        }));
+    const postsUrls = posts.map((post) => ({
+      url: `${baseUrl}/blog/${locale}/${post.slug}`,
+      lastModified: new Date(),
+    }));
 
-        const { data: categories } = await getBlogCategories(supabase, locale);
-        if (!categories) throw new Error("Error fetching categories");
+    const { data: categories } = await getBlogCategories(supabase, locale);
+    if (!categories) throw new Error("Error fetching categories");
 
-        const categoriesUrls = categories.map((category) => ({
-            url: `${baseUrl}/${locale}/categories/${category.slug}`,
-            lastModified: new Date(),
-        }));
+    const categoriesUrls = categories.map((category) => ({
+      url: `${baseUrl}/${locale}/categories/${category.slug}`,
+      lastModified: new Date(),
+    }));
 
-        sitemapUrls.push({
-            url: `${baseUrl}/${locale}`,
-            lastModified: new Date(),
-        });
-        sitemapUrls.push(...postsUrls, ...categoriesUrls);
-    }
+    sitemapUrls.push({
+      url: `${baseUrl}/${locale}`,
+      lastModified: new Date(),
+    });
+    sitemapUrls.push(...postsUrls, ...categoriesUrls);
+  }
 
-    return sitemapUrls;
+  return sitemapUrls;
 }
