4c9d3b30f6a798e5fbece7576d19aac63c80ea02
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-11-21 22:41:37 +0700

    #update: thuanvv update new architecture monorepo-v4

--- packages/supabase/utils/server.ts
+++ packages/supabase/utils/server.ts
@@ -5,31 +5,31 @@ import { cookies } from "next/headers";
 import { Database } from "./types";
 
 export async function supabaseServer() {
-    const cookieStore = await cookies();
+  const cookieStore = await cookies();
 
-    return createServerClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                get(name: string) {
-                    return cookieStore.get(name)?.value;
-                },
-                set(name: string, value: string, options: CookieOptions) {
-                    try {
-                        cookieStore.set({ name, value, ...options });
-                    } catch (error) {
-                        console.error(error);
-                    }
-                },
-                remove(name: string, options: CookieOptions) {
-                    try {
-                        cookieStore.set({ name, value: "", ...options });
-                    } catch (error) {
-                        console.error(error);
-                    }
-                },
-            },
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
         },
-    );
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options });
+          } catch (error) {
+            console.error(error);
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: "", ...options });
+          } catch (error) {
+            console.error(error);
+          }
+        },
+      },
+    },
+  );
 }
