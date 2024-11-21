f33d1a396f4d43c9e8af6ca200ea5a328fe43e64
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-11-21 22:40:07 +0700

    #update: thuanvv update new architecture monorepo-v4

--- packages/supabase/utils/server.ts
+++ packages/supabase/utils/server.ts
@@ -5,30 +5,31 @@ import { cookies } from "next/headers";
 import { Database } from "./types";
 
 export async function supabaseServer() {
-  const cookieStore = cookies();
-  return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
+    const cookieStore = await cookies();
+
+    return createServerClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                get(name: string) {
+                    return cookieStore.get(name)?.value;
+                },
+                set(name: string, value: string, options: CookieOptions) {
+                    try {
+                        cookieStore.set({ name, value, ...options });
+                    } catch (error) {
+                        console.error(error);
+                    }
+                },
+                remove(name: string, options: CookieOptions) {
+                    try {
+                        cookieStore.set({ name, value: "", ...options });
+                    } catch (error) {
+                        console.error(error);
+                    }
+                },
+            },
         },
-        set(name: string, value: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            console.error(error);
-          }
-        },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value: "", ...options });
-          } catch (error) {
-            console.error(error);
-          }
-        },
-      },
-    },
-  );
+    );
 }
