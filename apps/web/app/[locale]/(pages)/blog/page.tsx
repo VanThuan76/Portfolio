c0f2d1f77bc766d95bf82cb328a5ef323c6bf24a
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-11-25 17:27:43 +0700

    #update: thuanvv update new architecture monorepo-v5

--- apps/web/app/[locale]/(pages)/blog/page.tsx
+++ apps/web/app/[locale]/(pages)/blog/page.tsx
@@ -2,9 +2,9 @@ import { getTranslations } from "next-intl/server";
 
 import { getSupabaseBrowserClient } from "@repo/supabase/utils/client";
 import {
-    BlogCategorySuspbase,
-    getBlogCategories,
-    getBlogs,
+  BlogCategorySuspbase,
+  getBlogCategories,
+  getBlogs,
 } from "@repo/supabase/queries";
 
 import { IBlog } from "@repo/supabase/queries";
@@ -15,67 +15,67 @@ import { Tabs } from "@repo/design-system/components/molecules/tabs/tabs";
 import EmblaCarousel from "@repo/design-system/components/molecules/effects/embla-carousel";
 import CardBlog from "./components/card-blog";
 
-type Params = Promise<{ locale: string }>
+type Params = Promise<{ locale: string }>;
 
 export default async function Page({ params }: { params: Params }) {
-    const { locale } = await params;
+  const { locale } = await params;
 
-    const t = await getTranslations("pages.blog");
-    const supabase = getSupabaseBrowserClient();
+  const t = await getTranslations("pages.blog");
+  const supabase = getSupabaseBrowserClient();
 
-    const [blogs, blogCategories] = await Promise.all([
-        getBlogs(supabase, locale).then((res) => res.data),
-        getBlogCategories(supabase, locale).then((res) => res.data),
-    ]);
+  const [blogs, blogCategories] = await Promise.all([
+    getBlogs(supabase, locale).then((res) => res.data),
+    getBlogCategories(supabase, locale).then((res) => res.data),
+  ]);
 
-    return (
-        <div className="relative z-10 grid items-start justify-start w-full h-full grid-cols-1 gap-0 p-2 overflow-y-auto sm:p-4 md:gap-2 md:grid-cols-9 md:pt-0 md:px-6 pb-28 md:pb-0">
-            <div className="order-1 w-full col-span-1 pb-5 mx-auto h-fit md:order-2 md:col-span-8 md:py-5">
-                <div className="h-full [perspective:1000px] relative flex flex-col w-full items-start justify-start">
-                    <Tabs
-                        tabs={blogCategories.map((category: BlogCategorySuspbase) => ({
-                            title: category.name as string,
-                            value: category.name as string,
-                            content: (
-                                <CardBlog
-                                    items={blogs.filter(
-                                        (blog: IBlog) => blog.category_id === category.id,
-                                    )}
-                                    className="grid items-start justify-start grid-cols-1 gap-5 py-3 md:grid-cols-2 lg:grid-cols-4 md:gap-0"
-                                />
-                            ),
-                        }))}
-                    />
-                </div>
-            </div>
-            <div className="relative top-0 order-2 w-full space-y-4 h-fit md:order-1 md:sticky">
-                <div className="flex flex-col items-start justify-start order-1 w-full h-full col-span-1 gap-3 mx-auto mt-0 mb-5 overflow-hidden border-none md:mb-0 md:mt-6 md:border-r md:border-r-slate-400">
-                    <div className="hidden pb-2 border-b cursor-pointer md:block border-b-black dark:border-b-white">
-                        <p className="text-sm">{t("for_you")}</p>
-                    </div>
-                    <div className="hidden pb-2 border-b cursor-pointer md:block border-b-black dark:border-b-white">
-                        <p className="text-sm">{t("relevant")}</p>
-                    </div>
-                    <div className="hidden pb-2 border-b cursor-pointer md:block border-b-black dark:border-b-white">
-                        <p className="text-sm">{t("latest")}</p>
-                    </div>
-                    <span className="w-auto pb-1 mt-0 text-sm border-b md:w-full border-b-black">
-                        {t("references")}
-                    </span>
-                    <EmblaCarousel
-                        slides={GROUP_REFERENCES.map((item) => ({
-                            url: item.url as string,
-                            alt: item.alt,
-                        }))}
-                        options={{ dragFree: true, loop: true, slidesToScroll: 3 }}
-                        className="w-[50px] h-[50px] p-1 md:p-0 rounded-md"
-                    />
-                </div>
-                <div className="flex flex-col items-start justify-start w-full gap-2 h-fit">
-                    <span className="text-xs leading-4">{t("des_footer_1")}</span>
-                    <span className="text-xs leading-4">{t("des_footer_2")}</span>
-                </div>
-            </div>
+  return (
+    <div className="relative z-10 grid items-start justify-start w-full h-full grid-cols-1 gap-0 p-2 overflow-y-auto sm:p-4 md:gap-2 md:grid-cols-9 md:pt-0 md:px-6 pb-28 md:pb-0">
+      <div className="order-1 w-full col-span-1 pb-5 mx-auto h-fit md:order-2 md:col-span-8 md:py-5">
+        <div className="h-full [perspective:1000px] relative flex flex-col w-full items-start justify-start">
+          <Tabs
+            tabs={blogCategories.map((category: BlogCategorySuspbase) => ({
+              title: category.name as string,
+              value: category.name as string,
+              content: (
+                <CardBlog
+                  items={blogs.filter(
+                    (blog: IBlog) => blog.category_id === category.id,
+                  )}
+                  className="grid items-start justify-start grid-cols-1 gap-5 py-3 md:grid-cols-2 lg:grid-cols-4 md:gap-0"
+                />
+              ),
+            }))}
+          />
         </div>
-    );
+      </div>
+      <div className="relative top-0 order-2 w-full space-y-4 h-fit md:order-1 md:sticky">
+        <div className="flex flex-col items-start justify-start order-1 w-full h-full col-span-1 gap-3 mx-auto mt-0 mb-5 overflow-hidden border-none md:mb-0 md:mt-6 md:border-r md:border-r-slate-400">
+          <div className="hidden pb-2 border-b cursor-pointer md:block border-b-black dark:border-b-white">
+            <p className="text-sm">{t("for_you")}</p>
+          </div>
+          <div className="hidden pb-2 border-b cursor-pointer md:block border-b-black dark:border-b-white">
+            <p className="text-sm">{t("relevant")}</p>
+          </div>
+          <div className="hidden pb-2 border-b cursor-pointer md:block border-b-black dark:border-b-white">
+            <p className="text-sm">{t("latest")}</p>
+          </div>
+          <span className="w-auto pb-1 mt-0 text-sm border-b md:w-full border-b-black">
+            {t("references")}
+          </span>
+          <EmblaCarousel
+            slides={GROUP_REFERENCES.map((item) => ({
+              url: item.url as string,
+              alt: item.alt,
+            }))}
+            options={{ dragFree: true, loop: true, slidesToScroll: 3 }}
+            className="w-[50px] h-[50px] p-1 md:p-0 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col items-start justify-start w-full gap-2 h-fit">
+          <span className="text-xs leading-4">{t("des_footer_1")}</span>
+          <span className="text-xs leading-4">{t("des_footer_2")}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- apps/web/app/[locale]/(pages)/page.tsx
+++ apps/web/app/[locale]/(pages)/page.tsx
@@ -34,52 +34,51 @@
 //     { ssr: false },
 // );
 
-type Params = Promise<{ locale: string }>
+type Params = Promise<{ locale: string }>;
 
 export default async function Page({ params }: { params: Params }) {
+  return (
+    <div className="grid w-full h-full place-items-center">Comming soon</div>
+    // <div
+    //     className={cn(
+    //         "relative grid items-center justify-between w-full h-screen grid-cols-2 mx-auto overflow-hidden",
+    //         fontHome.className,
+    //     )}
+    //     data-lenis-prevent="true"
+    // >
+    //     <View className="absolute top-0 right-0 z-20 flex flex-col items-center justify-center w-full h-full">
+    //         <Suspense fallback={null}>
+    //             <Common />
+    //             <Model
+    //                 position={
+    //                     ["xs", "sm"].includes(breakpoint)
+    //                         ? [0, 30, -150]
+    //                         : [-30, 30, -150]
+    //                 }
+    //                 scale={
+    //                     ["xs", "sm"].includes(breakpoint) ? [7, 7, 7] : [10, 10, 10]
+    //                 }
+    //             />
+    //             <EffectComposerHandler />
+    //             <CameraRigHandler />
+    //         </Suspense>
+    //     </View>
 
-    return (
-        <div className="grid w-full h-full place-items-center">Comming soon</div>
-        // <div
-        //     className={cn(
-        //         "relative grid items-center justify-between w-full h-screen grid-cols-2 mx-auto overflow-hidden",
-        //         fontHome.className,
-        //     )}
-        //     data-lenis-prevent="true"
-        // >
-        //     <View className="absolute top-0 right-0 z-20 flex flex-col items-center justify-center w-full h-full">
-        //         <Suspense fallback={null}>
-        //             <Common />
-        //             <Model
-        //                 position={
-        //                     ["xs", "sm"].includes(breakpoint)
-        //                         ? [0, 30, -150]
-        //                         : [-30, 30, -150]
-        //                 }
-        //                 scale={
-        //                     ["xs", "sm"].includes(breakpoint) ? [7, 7, 7] : [10, 10, 10]
-        //                 }
-        //             />
-        //             <EffectComposerHandler />
-        //             <CameraRigHandler />
-        //         </Suspense>
-        //     </View>
+    //     <div className="absolute z-30 text-6xl italic font-black leading-none tracking-widest translate-x-1/2 md:text-9xl bottom-1/2 right-1/3 md:right-1/4">
+    //         <span className="text-white underline">AUSTIN</span>
+    //         <span className="block text-right">VU</span>
+    //     </div>
 
-        //     <div className="absolute z-30 text-6xl italic font-black leading-none tracking-widest translate-x-1/2 md:text-9xl bottom-1/2 right-1/3 md:right-1/4">
-        //         <span className="text-white underline">AUSTIN</span>
-        //         <span className="block text-right">VU</span>
-        //     </div>
-
-        //     <FlickeringGrid
-        //         className="absolute inset-0 z-0 w-full h-full"
-        //         squareSize={2}
-        //         gridGap={6}
-        //         color="#6B7280"
-        //         maxOpacity={0.3}
-        //         flickerChance={0.1}
-        //         height={1280}
-        //         width={1980}
-        //     />
-        // </div>
-    );
+    //     <FlickeringGrid
+    //         className="absolute inset-0 z-0 w-full h-full"
+    //         squareSize={2}
+    //         gridGap={6}
+    //         color="#6B7280"
+    //         maxOpacity={0.3}
+    //         flickerChance={0.1}
+    //         height={1280}
+    //         width={1980}
+    //     />
+    // </div>
+  );
 }
