56aba2a4539aecdd1b0b5d6fb9f0bc333b76458b
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2025-01-02 15:42:11 +0700

    #update: thuanvv update image blur

--- apps/web/app/(locale)/(pages)/blog/components/layout/footer.tsx
+++ apps/web/app/(locale)/(pages)/blog/components/layout/footer.tsx
@@ -12,55 +12,55 @@ import { useBreakpoint } from "@repo/hooks";
 import { BlurImage } from "@repo/design-system/components/molecules/ui-elements/blur-image";
 
 const Footer = () => {
-    const pathName = usePathname();
-    const breakpoint = useBreakpoint();
+  const pathName = usePathname();
+  const breakpoint = useBreakpoint();
 
-    const isSlugBlog = useMemo(
-        () => pathName.split("/").length >= 3 || ["xs", "sm"].includes(breakpoint),
-        [pathName, breakpoint],
-    );
+  const isSlugBlog = useMemo(
+    () => pathName.split("/").length >= 3 || ["xs", "sm"].includes(breakpoint),
+    [pathName, breakpoint],
+  );
 
-    return (
+  return (
+    <>
+      {!isSlugBlog && (
         <>
-            {!isSlugBlog && (
-                <>
-                    <div className="absolute z-0 flex -bottom-10 -left-5">
-                        {GROUP_PEOPLE_STATIC.map(
-                            ({ src, alt, translateX, translateY }, index) => (
-                                <BlurImage
-                                    key={index}
-                                    priority
-                                    alt={alt}
-                                    src={src}
-                                    blurDataURL={src ?? PLACE_HOLDER_BLUR_HASH}
-                                    className={cn(
-                                        "object-contain object-center transform",
-                                        translateX,
-                                        translateY,
-                                    )}
-                                    width={150}
-                                    height={150}
-                                    placeholder="blur"
-                                    sizes="(max-width: 150px) 150px, 150px"
-                                />
-                            ),
-                        )}
-                    </div>
-                    <BlurImage
-                        priority
-                        alt="@person_1"
-                        src="/images/blog/person_1.svg"
-                        blurDataURL={PLACE_HOLDER_BLUR_HASH}
-                        className="absolute z-0 object-contain object-center -rotate-180 -bottom-10 -right-10 -scale-y-100"
-                        width={150}
-                        height={150}
-                        placeholder="blur"
-                        sizes="(max-width: 150px) 150px, 150px"
-                    />
-                </>
+          <div className="absolute z-0 flex -bottom-10 -left-5">
+            {GROUP_PEOPLE_STATIC.map(
+              ({ src, alt, translateX, translateY }, index) => (
+                <BlurImage
+                  key={index}
+                  priority
+                  alt={alt}
+                  src={src}
+                  blurDataURL={src ?? PLACE_HOLDER_BLUR_HASH}
+                  className={cn(
+                    "object-contain object-center transform",
+                    translateX,
+                    translateY,
+                  )}
+                  width={150}
+                  height={150}
+                  placeholder="blur"
+                  sizes="(max-width: 150px) 150px, 150px"
+                />
+              ),
             )}
+          </div>
+          <BlurImage
+            priority
+            alt="@person_1"
+            src="/images/blog/person_1.svg"
+            blurDataURL={PLACE_HOLDER_BLUR_HASH}
+            className="absolute z-0 object-contain object-center -rotate-180 -bottom-10 -right-10 -scale-y-100"
+            width={150}
+            height={150}
+            placeholder="blur"
+            sizes="(max-width: 150px) 150px, 150px"
+          />
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default Footer;
