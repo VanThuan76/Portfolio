efd47c0b8fd7302dba42a236d26b74d568cd4d31
vuvanthuan <thuanvuvan76@gmail.com>
2026-03-04 01:14:01 +0700

    #fix: <thuan.vv> fix ux/ui home page

--- apps/web/app/(locale)/(pages)/(home)/components/switch-languages.tsx
+++ apps/web/app/(locale)/(pages)/(home)/components/switch-languages.tsx
@@ -5,16 +5,16 @@ import { useModal } from "@repo/hooks";
 import SwitchLanguageDoubleIcon from "./icons/switch-language-double";
 
 const SwitchLanguages = () => {
-  const { onOpen } = useModal();
+    const { onOpen } = useModal();
 
-  return (
-    <div
-      className="sticky cursor-pointer bottom-5 right-5 z-[1000000]"
-      onClick={() => onOpen("switch-languages")}
-    >
-      <SwitchLanguageDoubleIcon className="absolute bottom-5 right-5 w-[24px] h-[24px] md:w-[36px] md:h-[36px] " />
-    </div>
-  );
+    return (
+        <div
+            className="sticky cursor-pointer bottom-5 right-5 md:bottom-10 md:right-10 z-[1000000]"
+            onClick={() => onOpen("switch-languages")}
+        >
+            <SwitchLanguageDoubleIcon className="absolute bottom-5 right-5 w-[24px] h-[24px] md:w-[36px] md:h-[36px] " />
+        </div>
+    );
 };
 
 export default SwitchLanguages;
